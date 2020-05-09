ca9cf9d5c45c0c43fbb229b22916004abf38b211
aiken516 <51198185+aiken516@users.noreply.github.com>
2020-05-10 01:22:50 +0900

    Update Leibniz.java

--- Leibniz.java
+++ Leibniz.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Leibniz {
-	public float Leibniz() {
+	public Leibniz() {
 		float pi = 0;
 		
 		for (int i = 1; i < 1000000; i++) {
 			pi += Math.pow(-1, i+1) / (2*i - 1);
 		}
 		System.out.println(pi * 4);
-		return pi * 4;
 	}
 }
+
